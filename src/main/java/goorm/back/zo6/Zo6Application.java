afa7b246d6408e1c4d0157fca37095a66a071ada
sukangpunch <popora99@naver.com>
2025-03-01 01:37:33 +0900

    init: 초기화

---
+++ src/main/java/goorm/back/zo6/Zo6Application.java
@@ -0,0 +1,13 @@
+package goorm.back.zo6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zo6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zo6Application.class, args);
+    }
+
+}
