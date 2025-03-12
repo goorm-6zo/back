9a48d0b5596540327e8216a3dd4b352513afe573
Do-oya <qkrwjdgus1441@naver.com>
2025-03-12 14:51:08 +0900

    security 설정

--- src/main/java/goorm/back/zo6/auth/config/SecurityConfig.java
+++ src/main/java/goorm/back/zo6/auth/config/SecurityConfig.java
@@ -5,14 +5,20 @@ import goorm.back.zo6.auth.exception.CustomAccessDeniedHandler;
 import goorm.back.zo6.auth.exception.CustomAuthenticationEntryPoint;
 import goorm.back.zo6.auth.filter.JwtAuthFilter;
 import goorm.back.zo6.auth.util.JwtUtil;
+import goorm.back.zo6.auth.application.OAuth2LoginSuccessHandlerFactory;
+import goorm.back.zo6.user.application.OAuth2UserServiceFactory;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -21,44 +27,68 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@Slf4j
 public class SecurityConfig {
 
     private final JwtUtil jwtUtil;
     private final ObjectMapper objectMapper;
+    private final OAuth2UserServiceFactory oAuth2UserServiceFactory;
+    private final OAuth2LoginSuccessHandlerFactory successHandlerFactory;
 
     @Value("${server.url}")
     private String SERVER_URL;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        //cors 설정
-        http.cors((cors -> cors.configurationSource(configurationSource())));
-        // csfr disable
-        http.csrf((auth) -> auth.disable());
-        // form 로그인 disable
-        http.formLogin((auth) -> auth.disable());
-        // HTTP Basic 인증 방식 disable
-        http.httpBasic((auth) -> auth.disable());
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // CORS 설정
+        http.cors(cors -> cors.configurationSource(configurationSource()));
 
-        //경로별 인가 작업
-        http.authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**","/actuator/**").permitAll() // Swagger 관련 경로 허용
-                .requestMatchers("/api/v1/users/signup","/api/v1/auth/login").permitAll()
+        // CSRF 비활성화
+        http.csrf(csrf -> csrf.disable());
+
+        // Form 로그인 비활성화
+        http.formLogin(form -> form.disable());
+
+        // HTTP Basic 인증 비활성화
+        http.httpBasic(httpBasic -> httpBasic.disable());
+
+        // 경로별 인가 작업
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**", "/actuator/**").permitAll() // Swagger 관련 경로 허용
+                .requestMatchers("/oauth2/**", "/auth/login/kakao/**", "/auth/login/naver/**").permitAll() // OAuth2 관련 경로 허용
+                .requestMatchers("/api/v1/users/signup", "/api/v1/auth/login").permitAll()
                 .requestMatchers("/api/v1/rekognition/authentication").permitAll()
                 .anyRequest().authenticated());
 
-        //세션 설정 : STATELESS
-        http.sessionManagement((session) -> session
+        // 세션 설정 : STATELESS
+        http.sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         // JWTFilter 추가
-        http.addFilterBefore(new JwtAuthFilter(jwtUtil,objectMapper), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(new JwtAuthFilter(jwtUtil, objectMapper), UsernamePasswordAuthenticationFilter.class);
 
         // Exception handler 추가
         http.exceptionHandling(exceptionHandling ->
                 exceptionHandling
                         .accessDeniedHandler(new CustomAccessDeniedHandler(objectMapper))
                         .authenticationEntryPoint(new CustomAuthenticationEntryPoint(objectMapper)));
+
+        // OAuth2 로그인 설정
+        http.oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo.userService(oAuth2UserServiceFactory::loadUser))
+                        .successHandler((request, response, authentication) -> {
+                            if (authentication instanceof OAuth2AuthenticationToken oAuth2Token) {
+                                String provider = oAuth2Token.getAuthorizedClientRegistrationId().toUpperCase();
+                                log.debug("OAuth2 Provider: {}", provider);
+
+                                AuthenticationSuccessHandler handler = successHandlerFactory.getHandler(provider);
+                                handler.onAuthenticationSuccess(request, response, authentication);
+                            } else {
+                                log.error("OAuth2 Authentication Token이 아닙니다: {}", authentication.getClass().getName());
+                                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid authentication token");
+                            }
+                        })
+                );
         return http.build();
     }
 
@@ -66,12 +96,11 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
-        configuration.addAllowedOrigin(SERVER_URL);  // 특정 도메인 허용
+        configuration.addAllowedOrigin(SERVER_URL); // 특정 도메인 허용
         configuration.setAllowCredentials(true);
         configuration.addExposedHeader("ACCESS_TOKEN");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration); // 모든 주소요청에 위 설정을 넣어주겠다.
+        source.registerCorsConfiguration("/**", configuration); // 모든 요청에 적용
         return source;
     }
 }
-
