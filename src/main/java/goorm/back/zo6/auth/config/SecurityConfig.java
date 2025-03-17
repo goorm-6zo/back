94daadafbc01d87ecbb979423a2020676e0c2350
Do-oya <qkrwjdgus1441@naver.com>
2025-03-17 23:16:21 +0900

    관리자 권한으로 컨퍼런스, 세션 조회 추가

--- src/main/java/goorm/back/zo6/auth/config/SecurityConfig.java
+++ src/main/java/goorm/back/zo6/auth/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,6 +23,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -50,6 +52,7 @@ public class SecurityConfig {
                 .requestMatchers("/api/v1/reservation/temp").permitAll()
                 .requestMatchers("/api/v1/face/authentication").permitAll()
                 .requestMatchers("/api/v1/attendance/subscribe").permitAll()
+                .requestMatchers("/api/v1/admin/signup").permitAll()
                 .anyRequest().authenticated());
 
         //세션 설정 : STATELESS
