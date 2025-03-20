10f374fd36a17ab622c0f2bfb3d0d70bbfc9eb0e
Do-oya <qkrwjdgus1441@naver.com>
2025-03-20 15:11:21 +0900

    세션 구역만 수정할수 있게 변경

--- src/main/java/goorm/back/zo6/conference/application/SessionUpdateRequest.java
+++ src/main/java/goorm/back/zo6/conference/application/SessionUpdateRequest.java
@@ -3,17 +3,8 @@ package goorm.back.zo6.conference.application;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-
 @Getter
 @AllArgsConstructor
 public class SessionUpdateRequest {
-    private String name;
-    private Integer capacity;
     private String location;
-    private LocalDateTime time;
-    private String summary;
-    private String speakerName;
-    private String speakerOrganization;
-    private boolean isActive;
 }
