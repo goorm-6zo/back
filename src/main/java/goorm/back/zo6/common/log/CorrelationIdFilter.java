067d0e6864501bb29ba01b23074938e299cf3e9c
sukangpunch <popora99@naver.com>
2025-03-25 17:37:35 +0900

    로깅 클래스 추가

---
+++ src/main/java/goorm/back/zo6/common/log/CorrelationIdFilter.java
@@ -0,0 +1,22 @@
+package goorm.back.zo6.common.log;
+
+import jakarta.servlet.*;
+import org.slf4j.MDC;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE) // 가장 먼저 실행
+public class CorrelationIdFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String correlationId = UUID.randomUUID().toString(); // 요청마다 고유한  id 생성
+        MDC.put("CORRELATION_ID",correlationId);
+        MDC.clear();
+    }
+}
---
+++ src/main/java/goorm/back/zo6/common/log/TimeLogPointCut.java
@@ -0,0 +1,48 @@
+package goorm.back.zo6.common.log;
+
+import org.aspectj.lang.annotation.Pointcut;
+
+public class TimeLogPointCut {
+
+    @Pointcut("( attendPackagePointCut() || authPackagePointCut() || conferencePackagePointCut()" +
+            "|| facePackagePointCut() || noticePackagePointCut() || qrPackagePointCut()" +
+            "|| reservationPackagePointCut() || ssePackagePointCut() || userPackagePointCut())" +
+            "&& ( componentPointCut() || servicePointCut() || repositoryPointCut() )")
+    public void timeLogPointCut() {}
+
+    @Pointcut("execution(* goorm.back.zo6.attend..*.*(..))")
+    private void attendPackagePointCut() {}
+
+    @Pointcut("execution(* goorm.back.zo6.auth..*.*(..))")
+    private void authPackagePointCut() {}
+
+    @Pointcut("execution(* goorm.back.zo6.conference..*.*(..))")
+    private void conferencePackagePointCut() {}
+
+    @Pointcut("execution(* goorm.back.zo6.face..*.*(..))")
+    private void facePackagePointCut() {}
+
+    @Pointcut("execution(* goorm.back.zo6.notice..*.*(..))")
+    private void noticePackagePointCut() {}
+
+    @Pointcut("execution(* goorm.back.zo6.qr..*.*(..))")
+    private void qrPackagePointCut() {}
+
+    @Pointcut("execution(* goorm.back.zo6.reservation..*.*(..))")
+    private void reservationPackagePointCut() {}
+
+    @Pointcut("execution(* goorm.back.zo6.sse..*.*(..))")
+    private void ssePackagePointCut() {}
+
+    @Pointcut("execution(* goorm.back.zo6.user..*.*(..))")
+    private void userPackagePointCut() {}
+
+    @Pointcut("@target(org.springframework.stereotype.Service)")
+    private void servicePointCut() {}
+
+    @Pointcut("@target(org.springframework.stereotype.Repository)")
+    private void repositoryPointCut() {}
+
+    @Pointcut("@target(org.springframework.stereotype.Component)")
+    private void componentPointCut() {}
+}
