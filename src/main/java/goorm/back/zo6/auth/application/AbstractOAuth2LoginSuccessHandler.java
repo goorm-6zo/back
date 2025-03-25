e3029503ae057b8246c5360474a5b0f6713b3eee
Do-oya <qkrwjdgus1441@naver.com>
2025-03-25 15:04:32 +0900

    OAuth2 성공 핸들러 추가

---
+++ src/main/java/goorm/back/zo6/auth/application/AbstractOAuth2LoginSuccessHandler.java
@@ -0,0 +1,45 @@
+package goorm.back.zo6.auth.application;
+
+import goorm.back.zo6.auth.util.JwtUtil;
+import goorm.back.zo6.user.domain.Role;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.util.concurrent.TimeUnit;
+
+@RequiredArgsConstructor
+public abstract class AbstractOAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JwtUtil jwtUtil;
+
+    protected abstract String getEmail(OAuth2User oAuth2User);
+    protected abstract Long getUserId(OAuth2User oAuth2User);
+    protected abstract Role getRole(OAuth2User oAuth2User);
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+
+        Long userId = getUserId(oAuth2User);
+        String email = getEmail(oAuth2User);
+        Role role = getRole(oAuth2User);
+
+        String accessToken = jwtUtil.createAccessToken(userId, email, role);
+        Cookie cookie = createAccessTokenCookie(accessToken);
+        response.addCookie(cookie);
+    }
+
+    private Cookie createAccessTokenCookie(String accessToken) {
+        Cookie cookie = new Cookie("Authorization", accessToken);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath("/");
+        cookie.setMaxAge((int) TimeUnit.HOURS.toSeconds(2));
+        return cookie;
+    }
+}
